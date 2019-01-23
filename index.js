2200a3b2206bf53b3f5b2433a3fd0ae87acf9b34
Nguyen Trong Dai <ngtrdai290197@gmail.com>
2019-01-23 22:07:08 +0700

    Update port EVN

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running at http://localhost:${3000}`);
+app.listen(process.env.PORT || 8088, () => {
+    console.log(`Server is running at http://localhost:${8088}`);
 })
