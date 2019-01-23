0f1cb8664359a4f7897a54ab989174b37f436e49
Nguyen Trong Dai <ngtrdai290197@gmail.com>
2019-01-23 21:51:49 +0700

    Update connection string db

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,26 +1,26 @@
-// const mongoose = require('mongoose');
-
-// const dbConnection = () => {
-//     url = `mongodb://localhost:27017/CRUD_Demo`
-//     mongoose.Promise = global.Promise;
-//     mongoose.connect(url, { useNewUrlParser: true }).then(() => {
-//     }).catch(err => {
-//         console.log(err);
-//         process.exit();
-//     })
-// }
-
-// module.exports = { dbConnection: dbConnection };
-
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
+    url = `mongodb://localhost:27017/CRUD_Demo`
     mongoose.Promise = global.Promise;
-    url = 'mongodb://ngtrdai197:anhdaii1@ds211275.mlab.com:11275/server-web-api'
     mongoose.connect(url, { useNewUrlParser: true }).then(() => {
-    }).catch(() => {
+    }).catch(err => {
+        console.log(err);
         process.exit();
     })
 }
 
 module.exports = { dbConnection: dbConnection };
+
+// const mongoose = require('mongoose');
+
+// const dbConnection = () => {
+//     mongoose.Promise = global.Promise;
+//     url = 'mongodb://ngtrdai197:anhdaii1@ds211275.mlab.com:11275/server-web-api'
+//     mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+//     }).catch(() => {
+//         process.exit();
+//     })
+// }
+
+// module.exports = { dbConnection: dbConnection };
