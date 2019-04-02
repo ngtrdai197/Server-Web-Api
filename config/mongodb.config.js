ac156d57e5a06f1b0bc321f9be25d44d1aa53e25
Nguyen Dai <ngtrdai290197@gmail.com>
2019-04-02 22:37:58 +0700

    update db connection string

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -14,26 +14,26 @@
 
 
 
-const mongoose = require('mongoose');
-
-const dbConnection = () => {
-    mongoose.Promise = global.Promise;
-    const url = 'mongodb://ngtrdai197:anhdaii1@ds239911.mlab.com:39911/dbtest'
-    mongoose.connect(url, { useNewUrlParser: true }).then(() => {
-    }).catch(() => {
-        process.exit();
-    })
-}
-
 // const mongoose = require('mongoose');
 
 // const dbConnection = () => {
-//     const url = 'mongodb://ngtrdai197:anhdaii1@cluster0-shard-00-00-esass.mongodb.net:27017,cluster0-shard-00-01-esass.mongodb.net:27017,cluster0-shard-00-02-esass.mongodb.net:27017/RaoVat?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 //     mongoose.Promise = global.Promise;
+//     const url = 'mongodb://ngtrdai197:anhdaii1@ds239911.mlab.com:39911/dbtest'
 //     mongoose.connect(url, { useNewUrlParser: true }).then(() => {
 //     }).catch(() => {
 //         process.exit();
 //     })
 // }
+
+const mongoose = require('mongoose');
+
+const dbConnection = () => {
+    const url = 'mongodb://ngtrdai197:anhdaii1@cluster0-shard-00-00-esass.mongodb.net:27017,cluster0-shard-00-01-esass.mongodb.net:27017,cluster0-shard-00-02-esass.mongodb.net:27017/RaoVat?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+    mongoose.Promise = global.Promise;
+    mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+    }).catch(() => {
+        process.exit();
+    })
+}
 
 module.exports = { dbConnection: dbConnection };
